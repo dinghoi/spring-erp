f4f9157efe1100d186cf8edd76398854abf8aacb
dinghoi77 <dinghoi77@gmail.com>
2024-07-12 10:26:17 +0900

    fix : side nav text

--- src/main/java/com/mysite/spring_erp/board/repository/BoardRepository.java
+++ src/main/java/com/mysite/spring_erp/board/repository/BoardRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mysite.spring_erp.board.entity.EmpBoard;
 
 public interface BoardRepository extends JpaRepository<EmpBoard, Integer> {
-    // EmpBoard findByBoardTitleAndBoardContent(String title, String content);
+    EmpBoard findByTitleAndContent(String title, String content);
 
-    // List<EmpBoard> findByBoardTitleLike(String title);
+    List<EmpBoard> findByTitleLike(String title);
 }
